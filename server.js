afd6b8627272bbdcb8d1ca261284d8d26925e979
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-02 11:39:13 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = requrie('express');
+const path = require('path');
+const PORT = process.env.PORT || 8080
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// other api routes here //
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
