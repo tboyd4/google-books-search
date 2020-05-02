a4bfa4a226a45776b61c4e4ac097fce83974d3f0
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-02 12:05:34 -0500

    testing routes to stand in for API routes created

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Define middleware here
@@ -11,13 +11,23 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// other api routes here //
+// api routes
+app.get("/api/books", (req, res) => {
+  res.send("I will get the books");
+});
 
+app.post("/api/books", (req, res) => {
+  res.send("I will create a book");
+});
+
+app.delete("/api/books/:id", (req, res) => {
+  res.send("I will delete the book");
+});
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
-  });
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
